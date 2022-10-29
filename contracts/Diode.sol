95e72981cec4de338a57e081c4c0599bf871cdca
Alexandre Collette <alexcollette@hotmail.com>
2022-10-29 20:56:12 +0100

    GOERLI addresses

--- contracts/Diode.sol
+++ contracts/Diode.sol
@@ -6,7 +6,7 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/safeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
@@ -239,7 +239,6 @@ contract Diode is ERC721, Ownable {
             IERC20(suppliedAsset).safeTransfer(_msgSender(), amountOwed);
 
             return amountOwed;
-
         }
     }
 
@@ -282,8 +281,6 @@ contract Diode is ERC721, Ownable {
         } else {
             _apy = 0;
         }
-
-
     }
 
     // TODO: should do a preview deposit to check for APY evolution.
--- contracts/EulerStrat.sol
+++ contracts/EulerStrat.sol
@@ -6,7 +6,7 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/safeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 
 interface IEulerMarkets {
@@ -33,12 +33,14 @@ contract EulerStrat is Ownable {
     //  State Variables
     // -----------------
 
-    address EULER_MAINNET = 0x27182842E098f60e3D576794A5bFFb0777E025d3;
-    address EULER_MAINNET_MARKETS = 0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3;
-    address EULER_SIMPLELENS = 0x5077B7642abF198b4a5b7C4BdCE4f03016C7089C;
-    IEulerMarkets markets = IEulerMarkets(EULER_MAINNET_MARKETS);
-    // not enough time to calculate APY so hardcoded.
-    uint256 Lido_stETH_APY = 5400000000;
+    //address EULER_MAINNET = 0x27182842E098f60e3D576794A5bFFb0777E025d3;
+    //address EULER_MAINNET_MARKETS = 0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3;
+    //address EULER_SIMPLELENS_MAINNET = 0x5077B7642abF198b4a5b7C4BdCE4f03016C7089C;
+    
+    address EULER_GOERLI = 0x931172BB95549d0f29e10ae2D079ABA3C63318B3;
+    address EULER_GOERLI_MARKETS = 0x3EbC39b84B1F856fAFE9803A9e1Eae7Da016Da36;   
+    IEulerMarkets markets = IEulerMarkets(EULER_GOERLI_MARKETS);
+    
 
     address public underlyingToken;
 
@@ -65,8 +67,8 @@ contract EulerStrat is Ownable {
     function deposit (address token, uint256 amount) external onlyOwner {
         require(token == underlyingToken, "token supplied not supported by this contract");
         IERC20(underlyingToken).safeTransferFrom(_msgSender(), address(this), amount);
-        IERC20(underlyingToken).safeApprove(EULER_MAINNET, amount);
-        markets = IEulerMarkets(EULER_MAINNET_MARKETS);
+        IERC20(underlyingToken).safeApprove(EULER_GOERLI, amount);
+        markets = IEulerMarkets(EULER_GOERLI_MARKETS);
         IEulerEToken eToken = IEulerEToken(markets.underlyingToEToken(underlyingToken));
         eToken.deposit(0, amount);
 
@@ -86,14 +88,35 @@ contract EulerStrat is Ownable {
         totalInvested = eToken.balanceOf(address(this));
     }
 
-    function getSupplyAPY() external onlyOwner returns (uint256 _apy) {
-        (,,_apy) = (IEulerSimpleLens(EULER_SIMPLELENS).interestRates(underlyingToken));
 
-        _apy /= 10**16;
+    function getSupplyAPY() external view onlyOwner returns (uint256 _apy) {
+
+        // Not active on Goerli
+        //(,,_apy) = (IEulerSimpleLens(EULER_SIMPLELENS_MAINNET).interestRates(underlyingToken));
+
+        //_apy /= 10**16;
+
+        uint256 Lido_stETH_APY = 5400000000;
+        uint256 WBTC_APY = 2888888888;
+        uint256 USDT_APY = 3410000000;
+
+        address Lido_stETH_Goerli = 0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F;
+        address WBTC_Goerli = 0xC04B0d3107736C32e19F1c62b2aF67BE61d63a05;
+        address USDT_Goerli = 0xe583769738b6dd4E7CAF8451050d1948BE717679;
+
        
         // if underlyingToken = Lido stETH add LIDO APY. Hardcoded, not enough time to calculate.
-        if (underlyingToken == 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84) {
+        if (underlyingToken == Lido_stETH_Goerli) {
             _apy +=  Lido_stETH_APY;
+        }
+
+        // if underlyingToken = Lido stETH add LIDO APY. Hardcoded, not enough time to calculate.
+        if (underlyingToken == WBTC_Goerli) {
+            _apy +=  WBTC_APY;
+        }
+
+        if (underlyingToken == USDT_Goerli) {
+            _apy +=  USDT_APY;
         }
 
 
