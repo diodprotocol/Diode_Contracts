9067775b66167ea686a764028802e035c29138f5
Alexandre Collette <alexcollette@hotmail.com>
2022-11-13 13:31:47 +0100

    goerli setup

--- contracts/EulerStrat.sol
+++ contracts/EulerStrat.sol
@@ -31,13 +31,13 @@ contract EulerStrat is Ownable {
     //  State Variables
     // -----------------
 
-    address EULER_MAINNET = 0x27182842E098f60e3D576794A5bFFb0777E025d3;
-    address EULER_MAINNET_MARKETS = 0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3;
-    address EULER_SIMPLELENS_MAINNET = 0x5077B7642abF198b4a5b7C4BdCE4f03016C7089C;
+    //address EULER_MAINNET = 0x27182842E098f60e3D576794A5bFFb0777E025d3;
+    //address EULER_MAINNET_MARKETS = 0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3;
+    //address EULER_SIMPLELENS_MAINNET = 0x5077B7642abF198b4a5b7C4BdCE4f03016C7089C;
     
-    //address EULER_GOERLI = 0x931172BB95549d0f29e10ae2D079ABA3C63318B3;
-    //address EULER_GOERLI_MARKETS = 0x3EbC39b84B1F856fAFE9803A9e1Eae7Da016Da36;   
-    IEulerMarkets markets = IEulerMarkets(EULER_MAINNET_MARKETS);
+    address EULER_GOERLI = 0x931172BB95549d0f29e10ae2D079ABA3C63318B3;
+    address EULER_GOERLI_MARKETS = 0x3EbC39b84B1F856fAFE9803A9e1Eae7Da016Da36;   
+    IEulerMarkets markets = IEulerMarkets(EULER_GOERLI_MARKETS);
     
     address public underlyingToken;
 
@@ -63,8 +63,8 @@ contract EulerStrat is Ownable {
     function deposit (address token, uint256 amount) external onlyOwner {
         require(token == underlyingToken, "token supplied not supported by this contract");
         IERC20(underlyingToken).safeTransferFrom(_msgSender(), address(this), amount);
-        IERC20(underlyingToken).safeApprove(EULER_MAINNET, amount);
-        markets = IEulerMarkets(EULER_MAINNET_MARKETS);
+        IERC20(underlyingToken).safeApprove(EULER_GOERLI, amount);
+        markets = IEulerMarkets(EULER_GOERLI_MARKETS);
         IEulerEToken eToken = IEulerEToken(markets.underlyingToEToken(underlyingToken));
         eToken.deposit(0, amount);
     }
@@ -84,41 +84,33 @@ contract EulerStrat is Ownable {
 
 
     function getSupplyAPY() external view onlyOwner returns (uint256 _apy) {
-
         // Not active on Goerli
-        (,,_apy) = (IEulerSimpleLens(EULER_SIMPLELENS_MAINNET).interestRates(underlyingToken));
+/*         (,,_apy) = (IEulerSimpleLens(EULER_SIMPLELENS_MAINNET).interestRates(underlyingToken));
 
-        _apy /= 10**11;
+        _apy /= 10**11; */
 
-/*         uint256 Lido_stETH_APY = 5400000000;
         uint256 WBTC_APY = 2888888888;
-        uint256 USDT_APY = 3410000000;
-        uint256 WETH_APY = 1900000000;
+        uint256 USDT_APY = 5410000000;
+        uint256 WETH_APY = 5900000000;
 
-        address Lido_stETH_Goerli = 0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F;
-        address WBTC_Goerli = 0xC04B0d3107736C32e19F1c62b2aF67BE61d63a05;
-        address USDT_Goerli = 0xe583769738b6dd4E7CAF8451050d1948BE717679;
-        address WETH_Goerli = 0xa3401DFdBd584E918f59fD1C3a558467E373DacC; */
+        address WBTC_Goerli = 0xc49BB678a4d822f7F141D5bb4585d44cCe51e25E;
+        address USDT_Goerli = 0x7594a0368F18e666480Ad897612f28ad17435B4C;
+        address WETH_Goerli = 0xa3401DFdBd584E918f59fD1C3a558467E373DacC;
 
-       
-/*         // if underlyingToken = Lido stETH add LIDO APY. Hardcoded, not enough time to calculate.
-        if (underlyingToken == Lido_stETH_Goerli) {
-            _apy +=  Lido_stETH_APY;
-        }
 
-        // if underlyingToken = WBTC add LIDO APY. Hardcoded, not enough time to calculate.
+        // if underlyingToken = WBTC add WBTC APY. Hardcoded, not enough time to calculate.
         if (underlyingToken == WBTC_Goerli) {
-            _apy +=  WBTC_APY;
+            _apy =  WBTC_APY;
         }
 
         // if underlyingToken = USDT add LIDO APY. Hardcoded, not enough time to calculate.
         if (underlyingToken == USDT_Goerli) {
-            _apy +=  USDT_APY;
+            _apy =  USDT_APY;
         }
 
         // if underlyingToken = WETH add LIDO APY. Hardcoded, not enough time to calculate.
         if (underlyingToken == WETH_Goerli) {
-            _apy += WETH_APY;
-        } */
+            _apy = WETH_APY;
+        }
     }
 }
